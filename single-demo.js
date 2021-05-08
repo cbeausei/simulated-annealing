47def9653ee02e645c5d3d60aa485454419f478a
cbeausei <clementbeauseigneur@gmail.com>
2021-05-08 22:16:15 +0200

    Add second solver strat

--- single-demo.js
+++ single-demo.js
@@ -3,13 +3,14 @@ import {LitElement, html} from 'https://unpkg.com/lit-element/lit-element.js?mod
 const POINT_RAY = 4;
 const DELTA_BORDER = 10;
 const DRAW_DELAY_MS = 50;
-const SOLVE_DELAY_MS = 100;
+const SOLVE_DELAY_MS = 10;
 
 class SingleDemo extends LitElement {
   static get properties() {
     return {
       data: {type: Object},
       score: {type: Number},
+      solver: {type: Object},
     }
   }
 
@@ -19,21 +20,25 @@ class SingleDemo extends LitElement {
     this.newData = false;
     this.road = null;
     this.score = 0;
+    this.solver = {type: 0};
     setTimeout(() => this.drawData(), DRAW_DELAY_MS);
     setTimeout(() => this.solve(), 2 * SOLVE_DELAY_MS);
   }
 
   update(changedProperties) {
     super.update(changedProperties);
-    if (!changedProperties.has('data')) {
-      return;
+    if (changedProperties.has('data')) {
+      this.init();
     }
+  }
+
+  init() {
     this.newData = true;
     this.road = new Array();
     let score = 0;
     for (let i = 0; i < this.data.cities.length; ++i) {
-      this.road.push(i);
-      score += this.dist(i, (i + 1) % this.data.cities.length);
+      this.road.push((i + 1) % this.data.cities.length);
+      score += this.dist(i, this.road[i]);
     }
     this.score = score;
   }
@@ -66,9 +71,24 @@ class SingleDemo extends LitElement {
 
       <div container>
         <span>Score: ${this.score}</span>
+        <span>
+          <select id="solver-type-select" @change="${this.solverChange}">
+            <option value=0>nbr switch</option>
+            <option value=1>random move</option>
+          </select>
+        </span>
+        <span>
+          <button @click="${this.init}">Init</button>
+        </span>
         <canvas id="canvas"></canvas>
       </div>
     `
+  }
+
+  solverChange() {
+    this.solver = {
+      type: Number(this.shadowRoot.getElementById('solver-type-select').value),
+    };
   }
 
   dist(a, b) {
@@ -79,21 +99,54 @@ class SingleDemo extends LitElement {
 
   solve() {
     if (this.road?.length) {
-      const a = Math.floor(Math.random() * this.road.length);
-      const b = (a + 1) % this.road.length;
-      const z = (a + this.road.length - 1) % this.road.length;
-      const c = (b + 1) % this.road.length;
-      const eBefore = this.dist(this.road[z], this.road[a]) + this.dist(this.road[b], this.road[c]);
-      const eAfter = this.dist(this.road[z], this.road[b]) + this.dist(this.road[a], this.road[c]);
-      const energyReleased = eBefore - eAfter;
-      if (energyReleased > 0) {
-        const tmp = this.road[a];
-        this.road[a] = this.road[b];
-        this.road[b] = tmp;
-        this.score -= energyReleased;
+      switch (this.solver.type) {
+        case 0:
+          this.solveNbrSwap();
+          break;
+        case 1:
+          this.solveRndMove();
+          break;
+        default:
+          break;
       }
     }
     setTimeout(() => this.solve(), SOLVE_DELAY_MS);
+  }
+
+  solveNbrSwap() {
+    const z = Math.floor(Math.random() * this.road.length);
+    const a = this.road[z];
+    const b = this.road[a];
+    const c = this.road[b];
+    const eBefore = this.dist(z, a) + this.dist(b, c);
+    const eAfter = this.dist(z, b) + this.dist(a, c);
+    const energyReleased = eBefore - eAfter;
+    if (energyReleased > 0) {
+      this.road[z] = b;
+      this.road[b] = a;
+      this.road[a] = c;
+      this.score -= energyReleased;
+    }
+  }
+
+  solveRndMove() {
+    const a = Math.floor(Math.random() * this.road.length);
+    const b = this.road[a];
+    const c = this.road[b];
+    const x = Math.floor(Math.random() * this.road.length);
+    const y = this.road[x];
+    if (b === x || b === y) {
+      return;
+    }
+    const eBefore = this.dist(a, b) + this.dist(b, c) + this.dist(x, y);
+    const eAfter = this.dist(x, b) + this.dist(b, y) + this.dist(a, c);
+    const energyReleased = eBefore - eAfter;
+    if (energyReleased > 0) {
+      this.road[a] = c;
+      this.road[x] = b;
+      this.road[b] = y;
+      this.score -= energyReleased;
+    }
   }
 
   getCenter(city) {
@@ -135,7 +188,7 @@ class SingleDemo extends LitElement {
       this.ctx.fill();
     }
     for (let i = 0; i < this.road.length; ++i) {
-      this.printRoad(this.road[i], this.road[(i + 1) % this.road.length]);
+      this.printRoad(i, this.road[i]);
     }
     setTimeout(() => this.drawData(), DRAW_DELAY_MS);
   }
