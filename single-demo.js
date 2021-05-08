65cb5d4f418b74b2048a4f87b780e8f7ac576640
cbeausei <clementbeauseigneur@gmail.com>
2021-05-08 21:27:33 +0200

    Add greedy solver

--- single-demo.js
+++ single-demo.js
@@ -2,11 +2,14 @@ import {LitElement, html} from 'https://unpkg.com/lit-element/lit-element.js?mod
 
 const POINT_RAY = 4;
 const DELTA_BORDER = 10;
+const DRAW_DELAY_MS = 50;
+const SOLVE_DELAY_MS = 100;
 
 class SingleDemo extends LitElement {
   static get properties() {
     return {
       data: {type: Object},
+      score: {type: Number},
     }
   }
 
@@ -15,16 +18,24 @@ class SingleDemo extends LitElement {
     this.data = null;
     this.newData = false;
     this.road = null;
-    setTimeout(() => this.drawData(), 50);
+    this.score = 0;
+    setTimeout(() => this.drawData(), DRAW_DELAY_MS);
+    setTimeout(() => this.solve(), 2 * SOLVE_DELAY_MS);
   }
 
   update(changedProperties) {
     super.update(changedProperties);
+    if (!changedProperties.has('data')) {
+      return;
+    }
     this.newData = true;
     this.road = new Array();
+    let score = 0;
     for (let i = 0; i < this.data.cities.length; ++i) {
       this.road.push(i);
+      score += this.dist(i, (i + 1) % this.data.cities.length);
     }
+    this.score = score;
   }
 
   render() {
@@ -54,10 +65,35 @@ class SingleDemo extends LitElement {
       </style>
 
       <div container>
-        <span>Demo</span>
+        <span>Score: ${this.score}</span>
         <canvas id="canvas"></canvas>
       </div>
     `
+  }
+
+  dist(a, b) {
+    const cityA = this.data.cities[a];
+    const cityB = this.data.cities[b];
+    return Math.sqrt(Math.pow(cityA.x - cityB.x, 2) + Math.pow(cityA.y - cityB.y, 2));    
+  }
+
+  solve() {
+    if (this.road?.length) {
+      const a = Math.floor(Math.random() * this.road.length);
+      const b = (a + 1) % this.road.length;
+      const z = (a + this.road.length - 1) % this.road.length;
+      const c = (b + 1) % this.road.length;
+      const eBefore = this.dist(this.road[z], this.road[a]) + this.dist(this.road[b], this.road[c]);
+      const eAfter = this.dist(this.road[z], this.road[b]) + this.dist(this.road[a], this.road[c]);
+      const energyReleased = eBefore - eAfter;
+      if (energyReleased > 0) {
+        const tmp = this.road[a];
+        this.road[a] = this.road[b];
+        this.road[b] = tmp;
+        this.score -= energyReleased;
+      }
+    }
+    setTimeout(() => this.solve(), SOLVE_DELAY_MS);
   }
 
   getCenter(city) {
@@ -76,7 +112,6 @@ class SingleDemo extends LitElement {
   }
 
   drawData() {
-    setTimeout(() => this.drawData(), 50);
     this.canvas = this.shadowRoot.getElementById('canvas');
     const width = this.canvas.clientWidth;
     const height = this.canvas.clientHeight;
@@ -102,6 +137,7 @@ class SingleDemo extends LitElement {
     for (let i = 0; i < this.road.length; ++i) {
       this.printRoad(this.road[i], this.road[(i + 1) % this.road.length]);
     }
+    setTimeout(() => this.drawData(), DRAW_DELAY_MS);
   }
 }
 
